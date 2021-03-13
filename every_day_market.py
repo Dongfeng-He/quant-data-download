91093e20c758689df38f65bd79197d048bad295b
hedongfeng <hedongfeng@qingting.fm>
2021-03-14 00:21:23 +0800

    增加每日市场整体信息

--- every_day_market.py
+++ every_day_market.py
@@ -10,38 +10,80 @@ from multiprocessing import Pool
 import time
 import os
 
+
 auth(USER_NAME, PASSWORD)
 
-def 
-# 市场代码，分别为上海市场、上海A股、上海B股、深圳市场、深圳主板、中小企业板、创业板
-fields = ["322001", "322002", "322003", "322004", "322005", "322006", "322007", ]
 
-table = jq.finance.STK_EXCHANGE_TRADE_INFO
-cond1 = jq.finance.STK_EXCHANGE_TRADE_INFO.exchange_code == '322001'
-cond2 = jq.finance.STK_EXCHANGE_TRADE_INFO.date == '2015-01-07'
-query = jq.query(table).filter(cond1, cond2).limit(10)
-df = jq.finance.run_query(query)
-print()
+# 获取上海、深圳每日交易信息
+# total_market_cap(总市值列表)、circulating_market_cap(流通市值列表)、volume(成交股数列表)、money(成交金额列表)、deal_number(交易笔数列表)、pe_average(平均市盈率列表)、turnover_ratio(换手率列表)
+def get_sz_sh_trade_info(date):
+    # 总市值、流通市值、成交股数、成交金额、交易笔数、平均市盈率、换手率
+    fields = ["total_market_cap", "circulating_market_cap", "volume", "money", "deal_number", "pe_average", "turnover_ratio"]
+    # 市场代码，分别为上海市场、深圳市场、深圳主板、中小企业板、创业板
+    market_code_list = ["322001", "322004", "322005", "322006", "322007"]
+    field_data_dict = {field: [] for field in fields}
+    empty_df = pd.DataFrame(field_data_dict)
+    table = jq.finance.STK_EXCHANGE_TRADE_INFO
+    for market_code in market_code_list:
+        cond1 = jq.finance.STK_EXCHANGE_TRADE_INFO.exchange_code == market_code
+        cond2 = jq.finance.STK_EXCHANGE_TRADE_INFO.date == date
+        query = jq.query(table).filter(cond1, cond2).limit(10)
+        data_df = jq.finance.run_query(query)
+        for field in fields:
+            field_data_list = data_df[field].values.tolist()
+            if len(field_data_list) != 1:
+                return empty_df
+            field_data_dict[field].append(field_data_list[0])
+    result_df = pd.DataFrame({field: "|".join(list(map(lambda x: str(x), field_data_dict[field]))) for field in fields}, index=[0])
+    return result_df
 
 
+# 获取市场通每日交易信息
+# buy_amount(买入金额列表)、buy_volume(买入股数列表)、sell_amount(卖出金额列表)、sell_volume(卖出股数列表)，前者人民币，后者港币
+def get_sct_trade_info(date):
+    # 买入金额、买入股数、卖出金额、卖出股数
+    fields = ["buy_amount", "buy_volume", "sell_amount", "sell_volume"]
+    # 市场通代码，分别为沪股通、港股通（沪）
+    market_code_list = ["310001", "310003"]
+    field_data_dict = {field: [] for field in fields}
+    empty_df = pd.DataFrame(field_data_dict)
+    table = jq.finance.STK_ML_QUOTA
+    for market_code in market_code_list:
+        cond1 = jq.finance.STK_ML_QUOTA.link_id == market_code
+        cond2 = jq.finance.STK_ML_QUOTA.day == date
+        query = jq.query(table).filter(cond1, cond2).limit(10)
+        data_df = jq.finance.run_query(query)
+        for field in fields:
+            field_data_list = data_df[field].values.tolist()
+            if len(field_data_list) != 1:
+                return empty_df
+            field_data_dict[field].append(field_data_list[0])
+    result_df = pd.DataFrame({field: "|".join(list(map(lambda x: str(x), field_data_dict[field]))) for field in fields}, index=[0])
+    return result_df
 
 
+# 获取市场通每日汇率信息
+# refer_bid_rate(买入参考汇率)、refer_ask_rate(卖出参考汇率)、settle_bid_rate(买入结算汇率)、settle_ask_rate(卖出结算汇率)
+def get_sct_rate(date):
+    fields = ["refer_bid_rate", "refer_ask_rate", "settle_bid_rate", "settle_ask_rate"]
+    table = jq.finance.STK_EXCHANGE_LINK_RATE
+    cond1 = jq.finance.STK_EXCHANGE_LINK_RATE.link_id == "310003"   # 港股通（沪）
+    cond2 = jq.finance.STK_EXCHANGE_LINK_RATE.day == date
+    query = jq.query(table).filter(cond1, cond2).limit(10)
+    data_df = jq.finance.run_query(query)
+    if len(data_df) != 1:
+        return pd.DataFrame({field: [] for field in fields})
+    result_df = data_df[fields]
+    return result_df
 
 
+def get_day_info(date):
+    df_list = []
+    df_list.append(get_sz_sh_trade_info(date))
+    df_list.append(get_sct_trade_info(date))
+    df_list.append(get_sct_rate(date))
+    df = pd.concat(df_list, axis=1)
+    print()
 
-table = jq.finance.STK_HOLDER_NUM
-cond1 = jq.finance.STK_HOLDER_NUM.code == '000002.XSHE'
-cond2 = jq.finance.STK_HOLDER_NUM.pub_date > '2015-01-01'
-query = jq.query(table).filter(cond1, cond2).limit(10)
-df = jq.finance.run_query(query)
 
-
-security = "000001.XSHE"
-# "300015.XSHE" 爱尔眼科
-# "2015-03-01" 可测试返回空, "2015-03-01" 可测试正常数据
-# "2019-07-15"
-# date = "2015-03-01"
-# date = "2015-03-02"
-date = "2015-03-02"
-res = get_day_info(security, date)
-print()
+get_day_info("2015-03-02")
