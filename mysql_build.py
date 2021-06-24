ef0dbdb5c5c46e199dda1943fad116195d48aa72
hedongfeng <hedongfeng@qingting.fm>
2021-06-25 00:20:34 +0800

    增加 mtss 数据更新

--- mysql_build.py
+++ mysql_build.py
@@ -93,13 +93,81 @@ def batch_update_day_price_table(security_list, start_date, end_date):
     print(cost_time)
 
 
+def create_day_mtss_table(engine):
+    create_table_sql = """
+    CREATE TABLE IF NOT EXISTS `day_mtss` (
+        `record_id` INT NOT NULL AUTO_INCREMENT COMMENT '自增id',
+        `security` VARCHAR (15) NOT NULL COMMENT '股票代码',
+        `fin_value` DOUBLE DEFAULT NULL COMMENT '融资余额',
+        `fin_buy_value` DOUBLE DEFAULT NULL COMMENT '融资买入额',
+        `fin_refund_value` DOUBLE DEFAULT NULL COMMENT '融资偿还额',
+        `sec_value` DOUBLE DEFAULT NULL COMMENT '融券余量',
+        `sec_sell_value` DOUBLE DEFAULT NULL COMMENT '融券卖出量',
+        `sec_refund_value` DOUBLE DEFAULT NULL COMMENT '融券偿还量',
+        `fin_sec_value` DOUBLE DEFAULT NULL COMMENT '融资融券余额',
+        `date` DATE NOT NULL COMMENT '日期',
+        PRIMARY KEY (`record_id`),
+        UNIQUE `day_price_security_date_index` (`security`, `date`),
+        INDEX `day_price_security_index` (`security`),
+        INDEX `day_price_security_date` (`date`)
+    )ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '每日融资融券信息表';
+    """
+    engine.execute(create_table_sql)
+
+
+def batch_update_day_mtss_table(security_list, start_date, end_date):
+    # 不限制返回数量
+    # TODO: 还没有测 double、text 类型下 null 的插入是否正常
+    table_name = "day_mtss"
+    engine = mysql_connect()
+    create_day_mtss_table(engine)
+    date_list = get_trade_day_list(start_date=start_date, end_date=end_date)
+    retrieve_index_sql = "select security, date from day_mtss"
+    index_df = pd.read_sql_query(retrieve_index_sql, engine)
+    index_set = set((index_df["security"] + " " + index_df["date"].map(str)).tolist())
+    now = time.time()
+    for date in date_list:
+        df = get_securities_day_mtss(security_list, date)
+        concat_index_col = df["security"] + " " + df["date"]
+        keep_row = concat_index_col.apply(lambda x: True if x not in index_set else False)
+        df = df[keep_row]
+        if len(df) == 0:
+            continue
+        df.to_sql(table_name, engine, index=False, if_exists="append")
+    cost_time = time.time() - now
+    print(cost_time)
+
+
+def update_day_mtss_table(security_list, start_date, end_date):
+    table_name = "day_mtss"
+    engine = mysql_connect()
+    create_day_mtss_table(engine)
+    date_list = get_trade_day_list(start_date=start_date, end_date=end_date)
+    retrieve_index_sql = "select security, date from day_mtss"
+    index_df = pd.read_sql_query(retrieve_index_sql, engine)
+    index_set = set((index_df["security"] + " " + index_df["date"].map(str)).tolist())
+    now = time.time()
+    num = 0
+    for date in date_list:
+        for security in security_list:
+            df = get_day_mtss(security, date)
+            if len(df) == 0 or (df["security"] + " " + df["date"].map(str)).tolist()[0] in index_set:
+                continue
+            num += 1
+            df.to_sql(table_name, engine, index=False, if_exists="append")
+    cost_time = time.time() - now
+    print(cost_time)
+    print(cost_time / num)
+
+
 if __name__ == "__main__":
     auth(USER_NAME, PASSWORD)
     start_date = "2018-03-02"
     end_date = "2018-03-02"
-    # security_list = SECURITY_LIST
+    security_list = SECURITY_LIST
     # security_list = SECURITY_LIST[:300]
-    security_list = ["300015.XSHE", "000002.XSHE", "000022.XSHE", "000012.XSHE"]
+    # security_list = ["300015.XSHE", "000002.XSHE", "000022.XSHE", "000012.XSHE"]
     # update_day_price_table(security_list, start_date, end_date)
-    batch_update_day_price_table(security_list, start_date, end_date)
+    update_day_mtss_table(security_list, start_date, end_date)
+    # batch_update_day_mtss_table(security_list, start_date, end_date)
     print()
